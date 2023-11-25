eb0cf98029c9823eb1392f7cd63a67575b1856f8
GowaP <gowap@free.fr>
2023-11-25 18:19:42 +0100

    Correction du basculement entre « étoile » et « point » dans le JS. Correction de traduction en FR.

--- js/creator-23.js
+++ js/creator-23.js
@@ -103,7 +103,7 @@ async function resetCardIrregularities({ canvas = [1500, 2100, 0, 0], resetOther
 	if (resetOthers) {
 		//bottom info
 		await loadBottomInfo({
-			midLeft: { text: '{elemidinfo-set}·{elemidinfo-language}  {savex}{fontbelerenbsc}{fontsize' + scaleHeight(0.001) + '}{upinline' + scaleHeight(0.0005) + '}\uFFEE{elemidinfo-artist}', x: 0.0647, y: 0.9548, width: 0.8707, height: 0.0171, oneLine: true, font: 'gothammedium', size: 0.0171, color: 'white', outlineWidth: 0.003 },
+			midLeft: { text: '{elemidinfo-set} \u2022 {elemidinfo-language}  {savex}{fontbelerenbsc}{fontsize' + scaleHeight(0.001) + '}{upinline' + scaleHeight(0.0005) + '}\uFFEE{elemidinfo-artist}', x: 0.0647, y: 0.9548, width: 0.8707, height: 0.0171, oneLine: true, font: 'gothammedium', size: 0.0171, color: 'white', outlineWidth: 0.003 },
 			topLeft: { text: '{elemidinfo-number}  {loadx}{elemidinfo-rarity}  {elemidinfo-funcardeur}', x: 0.0647, y: 0.9377, width: 0.8707, height: 0.0171, oneLine: true, font: 'gothammedium', size: 0.0171, color: 'white', outlineWidth: 0.003 },
 			bottomLeft: { text: 'NOT FOR SALE', x: 0.0647, y: 0.9719, width: 0.8707, height: 0.0143, oneLine: true, font: 'gothammedium', size: 0.0143, color: 'white', outlineWidth: 0.003 },
 			wizards: { name: 'wizards', text: '{ptshift0,0.0172}\u2122 & \u00a9 {elemidinfo-year} Wizards of the Coast', x: 0.0647, y: 0.9377, width: 0.8707, height: 0.0167, oneLine: true, font: 'mplantin', size: 0.0162, color: 'white', align: 'right', outlineWidth: 0.003 },
